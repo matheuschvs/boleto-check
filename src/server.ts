c58de9ced2a8f1704d8bf81697a504e8559ab821
Matheus C <matheuschaves21@gmail.com>
2022-06-14 11:01:47 -0300

    feat: add error handler

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express'
 
+import { errorHandler } from './middlewares/errorHandler'
+
 import { boletosRoutes } from './routes/boletos.routes'
 
 const app = express()
 
 app.use('/boleto', boletosRoutes)
+
+app.use(errorHandler)
 
 app.listen(3333, () => console.log('Listening on port 3333'))
