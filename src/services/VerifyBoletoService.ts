46b467b7faa36eac7e39fcae060b77ad0e4d2880
Matheus C <matheuschaves21@gmail.com>
2022-06-16 17:17:46 -0300

    feat: add verification to former tests

--- src/services/VerifyBoletoService.ts
+++ src/services/VerifyBoletoService.ts
@@ -1,10 +1,18 @@
+import { AppError } from "../errors/AppError";
+
 interface IRequest {
   digitableLine: string;
 }
 
 class VerifyBoletoService {
   execute({ digitableLine }: IRequest) {
-    return { message: digitableLine }
+    if (!digitableLine.match(/^\d+$/)) {
+      throw new AppError('The digitable line must contain only numbers')
+    }
+
+    if (digitableLine.length > 48 || digitableLine.length < 47) {
+      throw new AppError('The digitable line must contain between 47 and 48 numbers')
+    }
   }
 }
 
